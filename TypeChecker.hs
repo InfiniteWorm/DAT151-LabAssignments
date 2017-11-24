097b530f5c084455346f5859bdd0116e5cab06a7
Ryan D. Widjaja <33755175+InfiniteWorm@users.noreply.github.com>
2017-11-24 15:32:09 +0100

    Update TypeChecker.hs

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE LambdaCase #-}
 module TypeChecker where
 
 import Control.Monad
@@ -16,17 +18,17 @@ import CPP.ErrM
 instance MonadError String Err where
   throwError msg = Bad msg
   catchError e h = case e of
-    Ok a    -> OK a
-	Bad msg -> h msg
+    Ok a    -> Ok a
+    Bad msg -> h msg
 
 type TC = ReaderT Sig (StateT Cxt Err)
 
 type Sig = Map Id SigEntry
 data SigEntry
   = FunSig
-    { returnType :: Type
-	, args       :: [Arg]
-	}
+   { returnType :: Type
+   , args       :: [Arg]
+   }
 
 data Cxt = Cxt
   { cxtReturnType :: Type
@@ -39,9 +41,9 @@ typecheck (PDefs defs) = do
   let sig = Map.fromList sigPairs
       sigPairs = map sigEntry defs ++
         [ (Id "printInt", FunSig Type_void [ ADecl Type_int undefined ]),
-		  (Id "printDouble", FunSig Type_void [ ADecl Type_double undefined ]),
-		  (Id "readInt", FunSig Type_int [ ADecl undefined ]),
-		  (Id "readDouble", FunSig Type_double [ADecl undefined ])
+        (Id "printDouble", FunSig Type_void [ ADecl Type_double undefined ]),
+        (Id "readInt", FunSig Type_int [ undefined ]),
+        (Id "readDouble", FunSig Type_double [undefined])
         ]
       sigEntry (DFun t f args _stms) = (f, FunSig t args)
   let names = map fst sigPairs
@@ -56,12 +58,12 @@ checkMain :: Sig -> Err ()
 checkMain sig = do
   case Map.lookup (Id "main") sig of
     Just (FunSig t args) -> do
-	  unless (t == Type_int) $ throwError $ "function main can only be declared with int"
-	  unless (null args) $ throwError $ "function main cannot take any arguments"
-	Nothing -> throwError $ "function main does not exists"
-	
+     unless (t == Type_int) $ throwError $ "function main can only be declared with int"
+     unless (null args) $ throwError $ "function main cannot take any arguments"
+    Nothing -> throwError $ "function main does not exists"
+   
 checkDefs :: [Def] -> TC ()
-checkDefs defs = mapM_ checkDef ds
+checkDefs defs = mapM_ checkDef defs
 
 checkDef :: Def -> TC ()
 checkDef (DFun t f args stms) = do
@@ -76,46 +78,58 @@ checkStms :: [Stm] -> TC ()
 checkStms stms = mapM_ checkStm stms
 
 checkStm :: Stm -> TC ()
-checkStm = \case
+checkStm stm = case stm of
   SExp e -> do
     _t <- inferExp e
-	return ()
+    return ()
+  SDecls t ids -> do
+    newVar id t
+    return ()
   s -> nyi s
 
 inferExp :: Exp -> TC Type
-inferExp = \case
+inferExp exp = case exp of
   EInt _ -> return Type_int
   EDouble _ -> return Type_double
   ETrue -> return Type_bool
   EFalse -> return Type_bool
   EPlus e1 e2 -> do
-    t' <- inferBin [Type_int, Type_double] e1 e2
-	return t'
+    t <- inferBin [Type_int, Type_double] e1 e2
+    return t
   EMinus e1 e2 -> do
-    t' <- inferBin [Type_int, Type_double] e1 e2
-	return t'
+    t <- inferBin [Type_int, Type_double] e1 e2
+    return t
+  ETimes e1 e2 -> do
+    t <- inferBin [Type_int, Type_double] e1 e2
+    return t
+  EDiv e1 e2 -> do
+    t <- inferBin [Type_int, Type_double] e1 e2
+    return t
   e@(EApp f es) -> do
     sig <- ask
-	case Map.lookup f sig of
-	  Nothing -> throwError $ "function undefined in : " ++ printTree e@
-	  Just (FunSig t args) -> do
-	    unless (length args == length es) $ throwError $ "wrong number of arguments in " ++ printTree e
-		let checkArg e (ADecl t _) = checkExp e t
-		zipWithM_ checkArg es args
-		return t
+    case Map.lookup f sig of
+     Nothing -> throwError $ "function undefined in : " ++ printTree e
+     Just (FunSig t args) -> do
+      unless (length args == length es) $ throwError $ "wrong number of arguments in " ++ printTree e
+      let checkArg e (ADecl t _) = checkExp e t
+      zipWithM_ checkArg es args
+      return t
   e -> nyi e
   
 inferBin :: [Type] -> Exp -> Exp -> TC Type
 inferBin types e1 e2 = do
   typ <- inferExp e1
   if elem typ types
-  then checkExp e2
-  else throwError $ "wrong type of expression " ++ printTree e1
+   then do
+     checkExp e2 typ
+     return typ
+   else 
+     throwError $ "wrong type of expression " ++ printTree e1
   
 checkExp :: Exp -> Type -> TC ()
 checkExp e t = do
   t' <- inferExp e
-  if (t/=t') then throwError $ "Expected type " ++ printTree t ++ ", got type " ++ printTree t'
+  unless (t==t') $ throwError $ "Expected type " ++ printTree t ++ ", got type " ++ printTree t'
  
 newVar :: Id -> Type -> TC ()
 newVar x t = do
