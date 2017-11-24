15d352f9be35790890caffa553ffc15c1999f870
Ryan D. Widjaja <33755175+InfiniteWorm@users.noreply.github.com>
2017-11-24 13:45:48 +0100

    Update TypeChecker.hs

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -1,7 +1,11 @@
 module TypeChecker where
 
 import Control.Monad
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Except
 
+import qualified Data.List as List
 import Data.Map (Map)
 import qualified Data.Map as Map
 
@@ -9,97 +13,116 @@ import CPP.Abs
 import CPP.Print
 import CPP.ErrM
 
+instance MonadError String Err where
+  throwError msg = Bad msg
+  catchError e h = case e of
+    Ok a    -> OK a
+	Bad msg -> h msg
 
-type Env = [[(Id, Type)]]
+type TC = ReaderT Sig (StateT Cxt Err)
 
---initialization of an empty environment stack
-emptyEnv :: Env
-emptyEnv = [[]]
+type Sig = Map Id SigEntry
+data SigEntry
+  = FunSig
+    { returnType :: Type
+	, args       :: [Arg]
+	}
 
---adding variable to the current scope
-addVar :: Env -> Type -> Id -> Err Env
-addVar (scope:rest) x t = 
-    case lookup x scope of
-      Nothing -> return (((x,t):scope):rest)
-      Just _  -> fail ("Variable " ++ printTree x ++ " already declared.")
+data Cxt = Cxt
+  { cxtReturnType :: Type
+  , cxtBlocks     :: [Block]
+  }
+type Block = Map Id Type
 
---searching a variable within a scope
-lookupVar :: Env -> Id -> Err Type
-lookupVar [] x = fail $ "Unknown variable " ++ printTree x ++ "."
-lookupVar (scope:rest) x = case lookup x scope of
-                             Nothing -> lookupVar rest x
-                             Just t  -> return t
-
---adding a scope to the stack							 
-addScope :: Env -> Env
-addScope env = []:env
-
---program is a sequence of function definitions
 typecheck :: Program -> Err ()
-typecheck (PDefs defs) = checkDefs emptyEnv defs
+typecheck (PDefs defs) = do
+  let sig = Map.fromList sigPairs
+      sigPairs = map sigEntry defs ++
+        [ (Id "printInt", FunSig Type_void [ ADecl Type_int undefined ]),
+		  (Id "printDouble", FunSig Type_void [ ADecl Type_double undefined ]),
+		  (Id "readInt", FunSig Type_int [ ADecl undefined ]),
+		  (Id "readDouble", FunSig Type_double [ADecl undefined ])
+        ]
+      sigEntry (DFun t f args _stms) = (f, FunSig t args)
+  let names = map fst sigPairs
+      dup   = names List.\\ List.nub names
+  unless (null dup) $ do
+    throwError $ "the following functions are defined several times: " ++
+      List.intercalate ", " (map printTree dup)
+  evalStateT (runReaderT (checkDefs defs) sig) (Cxt Type_void [])
+  checkMain sig
+ 
+checkMain :: Sig -> Err ()
+checkMain sig = do
+  case Map.lookup (Id "main") sig of
+    Just (FunSig t args) -> do
+	  unless (t == Type_int) $ throwError $ "function main can only be declared with int"
+	  unless (null args) $ throwError $ "function main cannot take any arguments"
+	Nothing -> throwError $ "function main does not exists"
+	
+checkDefs :: [Def] -> TC ()
+checkDefs defs = mapM_ checkDef ds
 
---function definitions can be more than 1
-checkDefs :: Env -> [Def] -> Err ()
-checkDefs env [] = return()
-checkDefs env (def:defs) = do def` <- checkDef env def
-							  checkDefs def` defs
+checkDef :: Def -> TC ()
+checkDef (DFun t f args stms) = do
+  put $ Cxt t [Map.empty]
+  checkArgs args
+  checkStms stms
 
---a single function definition has 1 or more arguments, and 1 or more statements							  
-checkDef :: Env -> Def -> Err ()
-checkDef env d = 
-	case d of
-		DFun t x args stms -> do addVar env x t
-								 checkArgs env args
-								 checkStms env stms
-								 
---arguments can be more than 1
-checkArgs :: Env -> [Arg] -> Err ()
-checkArgs env [] = return ()
-checkArgs env (arg:args) = do arg` <- checkArg env arg
-							  checkArgs arg` args
-							  
---a single argument behaves like variable declaration					
-checkArg :: Env -> Arg -> Err()
-checkArg env arg =
-	case a of
-		ADecl t x -> do if t == Type_void
-						then putStrLn "Argument cannot be declared with void"
-						else addvar env x t
+checkArgs :: [Arg] -> TC ()
+checkArgs args = mapM_ (\ (ADecl t x) -> newVar x t) args
 
---there can be more than 1 statements within a function body
-checkStms :: Env -> [Stm] -> Err ()
-checkStms env [] = return ()
-checkStms env (st:stms) = do env` <- checkStm env st
-                             checkStms env` stms
+checkStms :: [Stm] -> TC ()
+checkStms stms = mapM_ checkStm stms
 
---types of statements
-checkStm :: Env -> Stm -> Err Env
-checkStm env s = 
-    case s of
-      SDecls t x      -> addVar env t x
+checkStm :: Stm -> TC ()
+checkStm = \case
+  SExp e -> do
+    _t <- inferExp e
+	return ()
+  s -> nyi s
 
-
---checking whether a type of an expression matches the given type from parameter
-checkExp :: Env -> Exp -> Type -> Err ()
-checkExp env e t = 
-    do t' <- inferExp env e
-       if t' /= t 
-         then fail (printTree e ++ " has type " ++ printTree t'
-                    ++ " expected " ++ printTree t)
-         else return ()
-
---type inference for expressions		 
-inferExp :: Env -> Exp -> Err Type
-inferExp env e = 
-    case e of
-      EId x         -> lookupVar env x
-      EInt _         -> return Type_int
-      EDouble _      -> return Type_double
-      EPlus e1 e2    -> do t1 <- inferExp env e1
-                           t2 <- inferExp env e2
-                           if t1 == t2 
-                             then return t1
-                             else fail (printTree e1 ++ " has type " ++ printTree t1
-                                         ++ " but " ++ printTree e2 
-                                         ++ " has type " ++ printTree t2)
-
+inferExp :: Exp -> TC Type
+inferExp = \case
+  EInt _ -> return Type_int
+  EDouble _ -> return Type_double
+  ETrue -> return Type_bool
+  EFalse -> return Type_bool
+  EPlus e1 e2 -> do
+    t' <- inferBin [Type_int, Type_double] e1 e2
+	return t'
+  EMinus e1 e2 -> do
+    t' <- inferBin [Type_int, Type_double] e1 e2
+	return t'
+  e@(EApp f es) -> do
+    sig <- ask
+	case Map.lookup f sig of
+	  Nothing -> throwError $ "function undefined in : " ++ printTree e@
+	  Just (FunSig t args) -> do
+	    unless (length args == length es) $ throwError $ "wrong number of arguments in " ++ printTree e
+		let checkArg e (ADecl t _) = checkExp e t
+		zipWithM_ checkArg es args
+		return t
+  e -> nyi e
+  
+inferBin :: [Type] -> Exp -> Exp -> TC Type
+inferBin types e1 e2 = do
+  typ <- inferExp e1
+  if elem typ types
+  then checkExp e2
+  else throwError $ "wrong type of expression " ++ printTree e1
+  
+checkExp :: Exp -> Type -> TC ()
+checkExp e t = do
+  t' <- inferExp e
+  if (t/=t') then throwError $ "Expected type " ++ printTree t ++ ", got type " ++ printTree t'
+ 
+newVar :: Id -> Type -> TC ()
+newVar x t = do
+  block <- gets (head . cxtBlocks)
+  when (Map.member x block) $ throwError $ "variable " ++ printTree x ++ " has already been declared"
+  let block' = Map.insert x t block
+  modify $ \ cxt -> cxt { cxtBlocks = block' : tail (cxtBlocks cxt) }
+  
+nyi :: Show a => a -> TC b
+nyi a = throwError $ "not yet implemented: checking " ++ show a
