f1c229a53dc48fe4fad309e1e27ad23c28c059e0
Ryan D. Widjaja <33755175+InfiniteWorm@users.noreply.github.com>
2017-12-14 15:15:55 +0100

    Update Compiler.hs

--- Compiler.hs
+++ Compiler.hs
@@ -24,11 +24,14 @@ import Data.Maybe
 import Data.Map(Map)
 import qualified Data.Map as Map
 
+import System.Process
 import System.Process (callProcess)
-import System.FilePath.Posix (addExtension)
+import System.FilePath
+import System.Directory
 
 import CPP.Abs
 import CPP.Print
+
 
 type Compile = RWS Sigc Output St
 
@@ -135,7 +138,7 @@ instance ToJVM Label where
 builtIn :: [(Id, Fun)]
 builtIn = 
     [ (Id "printInt", Fun (Id "Runtime/printInt") $ FunType Type_void [Type_int]),
-      (Id "readInt", Fun (Id "Runtime/readInt") $ FunType Type_void [])
+      (Id "readInt", Fun (Id "Runtime/readInt") $ FunType Type_int [])
     ]
 
 funType :: Def -> FunType
@@ -197,8 +200,11 @@ instance ToJVM Code where
         Pop t -> "pop"
         Dup -> "dup"
         Return t -> prefix t ++ "return"
-        Call f -> "invokestatic" ++ toJVM f
-        IConst i -> "ldc " ++ show i
+        Call f -> "invokestatic " ++ toJVM f
+        IConst i -> 
+            if i <= 5
+                then "iconst_" ++ show i
+                else "ldc " ++ show i
         Inc t a i -> prefix t ++ "inc " ++ show a ++ " " ++ show i
         Add t -> prefix t ++ "add"
         Sub t -> prefix t ++ "sub"
@@ -206,7 +212,7 @@ instance ToJVM Code where
         Mul t -> prefix t ++ "mul"
         Bipush i -> "bipush " ++ show i
         IfEq lbl -> "if_icmpeq " ++ toJVM lbl
-        IfNeq lbl -> "if_icmne " ++ toJVM lbl
+        IfNeq lbl -> "if_icmpne " ++ toJVM lbl
         IfGe lbl -> "if_icmpge " ++ toJVM lbl
         IfLe lbl -> "if_icmple " ++ toJVM lbl
         IfGt lbl -> "if_icmpgt " ++ toJVM lbl
@@ -222,7 +228,6 @@ emit :: Code -> Compile ()
 emit (Store Type_void _) = return ()
 emit (Load Type_void _) = return()
 emit (Pop Type_void) = return()
-
 emit c = do
     tell [toJVM c]
     case c of
@@ -232,6 +237,7 @@ emit c = do
         Return t -> decStack t
         Call f -> decStack f
         IConst _ -> incStack Type_int
+        Inc t _ _ -> incStack t
         Add t -> decStack t
         Sub t -> decStack t
         Div t -> decStack t
@@ -247,28 +253,38 @@ emit c = do
         Dup -> incStack Type_int
         And _ -> decStack Type_int
         Or _ -> decStack Type_int
-        Lbl _ -> incStack Type_void
-        Goto _ -> incStack Type_void
+        Lbl _ -> return()
+        Goto _ -> return()
 
 ------------------
---add limitlocal every time a variable is declared
 newVar :: Id -> Type -> Compile ()
 newVar x t = do
     headblock <- gets (head . cx)
     tailblock <- gets (tail . cx)
+    --get current limitLocals
     currentlocal <- gets(limitLocals)
+    --get the current available address
     currentAddr <- gets(nextAddr)
+
     let newlocal = currentlocal + 1
     let block' = Map.insert x currentAddr headblock
-    --increase the next available address by size t
+
+    --updating the next available address
     let nextAddress = currentAddr + size t
     modify $ \cxt -> cxt {cx = block' : tailblock,limitLocals = newlocal, nextAddr = nextAddress}
     return ()
 
-newLabel :: Label -> Compile ()
-newLabel (L {theLabel = tl}) = do
-    let tl' = tl + 1
-    modify $ \cxt -> cxt {nextLabel = L tl'} 
+newLabel2 :: Compile Label
+newLabel2 = do
+    lbl <- gets nextLabel
+    case lbl of
+        L 0 -> do
+            modify $ \cxt -> cxt {nextLabel = L 1}
+            return $ L 0
+        L i -> do
+            let i' = i + 1
+            modify $ \cxt -> cxt {nextLabel = L i'} 
+            return $ L i
 
 
 -- | Return address of a variable.
@@ -299,22 +315,26 @@ newBlock = do
 popBlock :: Compile ()
 popBlock = do
     block <- gets (cx)
-    let bl' = tail (block)
-    modify $ \st -> st {cx = bl'}
+    add <- gets(nextAddr)
+    let headbl = head (block)
+    let tailbl = tail (block)
+    
+    modify $ \st -> st {cx = tailbl, nextAddr = add - length headbl}
     return ()
 
 compile :: String -> Program -> IO ()
 compile name prg@(PDefs defs) = do
     let sig = Map.fromList $
             builtIn ++
-            map (\ def@(DFun _ f@(Id x) _ _ ) -> (f, Fun (Id $ name ++ "/" ++ x) (funType def) )) defs
-    let ((), w) = evalRWS (compileProgram name prg) sig initSt
+            map (\ def@(DFun _ f@(Id x) _ _ ) -> (f, Fun (Id $ takeFileName name ++ "/" ++ x) (funType def) )) defs
+    let ((), w) = evalRWS (compileProgram (takeFileName name) prg) sig initSt
     -- Print to stdout.
-    mapM_ putStrLn w
+    --mapM_ putStrLn w
     -- Write to .j file and call jasmin
     let jfile = addExtension name "j"
     writeFile jfile $ unlines w
-    --callProcess "jasmin" [jfile]
+    callProcess "java" ["-jar","jasmin.jar",jfile]
+    renameFile ((takeFileName name) ++ ".class") (addExtension name ".class")
     
 -- | Compiling the program.
     
@@ -326,7 +346,7 @@ compileProgram name (PDefs defs) = do
     header =
         [ ";; BEGIN HEADER"
         , ""
-        , ".class public " ++ name
+        , ".class public " ++ takeFileName name
         , ".super java/lang/Object"
         , ""
         , ".method public <init>()V"
@@ -342,7 +362,7 @@ compileProgram name (PDefs defs) = do
         , "  .limit locals 1"
         , "  .limit stack  1"
         , ""
-        , "  invokestatic " ++ name ++ "/main()I"
+        , "  invokestatic " ++ takeFileName name ++ "/main()I"
         , "  pop"
         , "  return"
         , ""
@@ -379,56 +399,69 @@ compileFun def@(DFun t f args ss) = do
     tell $ map (\ s -> if null s then s else "  " ++ s) w
 
     -- function footer
+    if t == Type_void
+        then emit(Return Type_void)
+        else do
+            emit (IConst 0)
+            emit (Return Type_int)
     tell [ "", ".end method"]
 
 compileStm :: Stm -> Compile ()
-compileStm s = do
+compileStm s =
     case s of
         SExp e -> do
             compileExp e
-            emit(Pop Type_int)
-            return ()
+            case e of 
+                EApp _ _ -> return()
+                otherwise -> emit (Pop Type_int)
         SDecls t ids -> do
-            forM_ ids $ \id -> do
-                newVar id t
-            return ()
+            forM_ ids $ \id -> newVar id t
         SInit t id exp -> do
             --declare new variable
             newVar id t
             --compile the exp
             compileExp exp
-            --store & load to the declared variable
+            --store to the declared variable
             block <- gets(cx)
             a <- lookupVar id block
             emit(Store t a)
-            emit(Load t a)
-            return ()
         SReturn exp -> do
             compileExp exp           
             rType <- gets(retType) 
             emit (Return rType)
             return ()
-        SWhile exp stm -> do
-            init <- gets nextLabel
-            newLabel init
-            lb1 <- gets nextLabel
-            emit(Lbl init)
+        SIfElse exp stm1 stm2 -> do
+            false <- newLabel2
+            true <- newLabel2
             compileExp exp
-            emit(IfEqZ lb1)
+            emit(IfEqZ false)
+            compileStm stm1
+            emit(Goto true)
+            emit(Lbl false)
+            compileStm stm2
+            emit(Lbl true)
+        SBlock stms -> do
+            newBlock
+            mapM_ compileStm stms
+            popBlock
+        SWhile exp stm -> do
+            test <- newLabel2
+            end <- newLabel2
+            emit (Lbl test)
+            compileExp exp
+            emit (IfEqZ end)
             compileStm stm
-            emit(Goto init)
-            emit(Lbl lb1)
-        --need to do : SWhile, SBlock, SIfElse
-        s -> nyi s
+            emit (Goto test)
+            emit (Lbl end)
 
 compileExp :: Exp -> Compile ()
-compileExp e = do
+compileExp e =
     case e of
         ETrue -> do
-            emit(Bipush 1)
+            emit(IConst 1)
             return()
         EFalse -> do
-            emit(Bipush 0)
+            emit(IConst 0)
             return ()
         EInt i -> do
             emit (IConst i)
@@ -460,23 +493,107 @@ compileExp e = do
             emit(Store Type_int a)
             emit(Load Type_int a)
         ELt e1 e2 -> do
-            label <- gets nextLabel
-            newLabel label
-
-            emit(Bipush 1)
+            emit(IConst 1)
             compileExp (e1)
             compileExp (e2)
-
-            label' <- gets nextLabel
-
-            emit(IfLt label')
+            true <- newLabel2
+            emit (IfLt true)
             emit (Pop Type_int)
-            emit (Bipush 0)
-            emit (Lbl label')
+            emit (IConst 0)
+            emit (Lbl true)
+        ELtEq e1 e2 -> do
+            emit(IConst 1)
+            compileExp (e1)
+            compileExp (e2)
+            true <- newLabel2
+            emit (IfLe true)
+            emit (Pop Type_int)
+            emit (IConst 0)
+            emit (Lbl true)
+        EGt e1 e2 -> do
+            emit(IConst 1)
+            compileExp (e1)
+            compileExp (e2)
+            true <- newLabel2
+            emit (IfGt true)
+            emit (Pop Type_int)
+            emit (IConst 0)
+            emit (Lbl true)   
+        EGtEq e1 e2 -> do
+            emit(IConst 1)
+            compileExp (e1)
+            compileExp (e2)
+            true <- newLabel2
+            emit (IfGe true)
+            emit (Pop Type_int)
+            emit (IConst 0)
+            emit (Lbl true) 
+        EEq e1 e2 -> do
+            emit(IConst 1)
+            compileExp (e1)
+            compileExp (e2)
+            true <- newLabel2
+            emit (IfEq true)
+            emit (Pop Type_int)
+            emit (IConst 0)
+            emit (Lbl true) 
+        ENEq e1 e2 -> do
+            emit(IConst 1)
+            compileExp (e1)
+            compileExp (e2)
+            true <- newLabel2
+            emit (IfNeq true)
+            emit (Pop Type_int)
+            emit (IConst 0)
+            emit (Lbl true) 
+        EAnd e1 e2 -> do --lazy evaluation on && operator
+            compileExp(e1)
+            true <- newLabel2
+            false <- newLabel2
+            emit (IfEqZ true)
+            emit (IConst 1)
+            compileExp(e2)
+            emit (And Type_int)
+            emit (Goto false)
+            emit (Lbl true)
+            emit (IConst 0)
+            emit (Lbl false)
+        EOr e1 e2 -> do --lazy evaluation on || operator
+            compileExp(e1)
+            emit(IConst 1)
+            true <- newLabel2
+            false <- newLabel2
+            emit (IfEq true)
+            emit (IConst 0)
+            compileExp(e2)
+            emit(Or Type_int)
+            emit (Goto false)
+            emit (Lbl true)
+            emit (IConst 1)
+            emit (Lbl false)
         EPreIncr id -> do
+            block <- gets(cx)
+            a <- lookupVar id block
+            emit (Inc Type_int a 1)
+            emit (Load Type_int a)
+        EPostIncr id -> do
             block <- gets(cx)
             a <- lookupVar id block
             emit (Load Type_int a)
             emit (Inc Type_int a 1)
-        --Need to do : EApp, EPre/Post Inc/Decr, and E Lt/Gt/Le/Ge/And/Or
-        e -> nyi e
+        EPostDecr id -> do
+            block <- gets(cx)
+            a <- lookupVar id block
+            emit (Load Type_int a)
+            emit (Inc Type_int a (-1))
+        EPreDecr id -> do
+            block <- gets(cx)
+            a <- lookupVar id block
+            emit (Inc Type_int a $ -1)
+            emit (Load Type_int a)
+        EApp fname args -> do
+            sig <- ask
+            forM_ args $ \arg -> compileExp(arg)
+            case Map.lookup fname sig of
+                Just f -> emit (Call f)
+        --e -> nyi e
